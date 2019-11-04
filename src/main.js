0623515dac167abb1e774a4ea5c891d4ed7fc643
Justin Struk <strukjustin@gmail.com>
2019-11-04 14:10:18 -0500

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -8,3 +8,26 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+export const getCusts = async url => {// eslint-disable-line no-unused-vars
+  const response = await axios.get("https://localhost:44383/Customers");
+  console.log(response.data);
+  return response.data;
+};
+
+
+export const getCustomerDetails = async id => {// eslint-disable-line no-unused-vars
+  const response = await axios.get("https://localhost:44383/Customers/" + id);
+  return response.data;
+};
+
+export const updateCustomer = async (_id, _firstName, _lastName, _phoneNumber) => {// eslint-disable-line no-unused-vars
+  const response = axios.put('/Customers', {
+    firstName: _firstName,
+    lastName: _lastName,
+    phoneNumber: _phoneNumber
+  })
+  return response.data;
+ // const response = await axios.put("https://localhost:44383/Customers/" + id);
+ // return response.data;
+};
